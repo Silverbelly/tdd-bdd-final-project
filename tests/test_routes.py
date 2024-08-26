81a4bb64173d12c4022bca4cc1bfd10bec584268
Jon Noel <jonnoel@att.net>
2024-08-26 17:16:25 -0400

    Routes testing part 1

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -166,6 +166,15 @@ class TestProductRoutes(TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+    def test_get_product(self):
+        """ It should get a product """
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        print(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+        self.assertEqual(data["name"], test_product.name)
+        self.assertEqual(data["description"], test_product.description)
 
     ######################################################################
     # Utility functions
